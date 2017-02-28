3dda608187b70165c48906d34cec7ca2c89f112f
root <guilhermebritto.prof@gmail.com>
2017-02-28 09:16:16 -0300

    correção de updateAt

--- App/Api/Controller/AssociadoController.php
+++ App/Api/Controller/AssociadoController.php
@@ -74,7 +74,7 @@ class AssociadoController implements Controller {
 	//Update de cadastro
 	public function atulizaCadastro($data){
 		$associado = new Associado($data);
-		$associado->createAt = date('Y-m-d H:i:s');
+		$associado->updateAt = date('Y-m-d H:i:s');
 		try{
 			Log::Message("Atualizando informações do Associado");
 			return $associado->update();
@@ -87,7 +87,7 @@ class AssociadoController implements Controller {
 	public function inativar($id){
 		$associado = new Associado();
 		$associado->id = $id;
-		$associado->createAt = date('Y-m-d H:i:s');
+		$associado->updateAt = date('Y-m-d H:i:s');
 		$associado->status = 'INATIVO';
 		try{
 			Log::Message("Inativando Associado ".$id);
@@ -133,9 +133,8 @@ class AssociadoController implements Controller {
 		try{
 			Log::Message("Listando Associados Em Espera");
 			return $associado->select(array('inner' => 
-				array('vaga' => array('associado.id' => 'vaga.associado_id'),
-					'periodo' => array('vaga.periodo_id' => 'periodo.id')
-					
+				array('vaga' 	=> array('associado.id' 	=> 'vaga.associado_id'),
+					  'periodo' => array('vaga.periodo_id'  => 'periodo.id')
 					)
 				)
 			);
