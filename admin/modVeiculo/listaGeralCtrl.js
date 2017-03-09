01b01aa36028524e8a1337bafd8c2f9b5d3cdee8
adnilton <adniltonweb@gmail.com>
2017-03-08 23:02:07 -0300

    updae lista geral

--- admin/modVeiculo/listaGeralCtrl.js
+++ admin/modVeiculo/listaGeralCtrl.js
@@ -2,9 +2,8 @@ app.controller("listaGeralCtrl", function($scope, $http,$location , $timeout,$se
   //Pega o Id do Usuario Logado
   var idUsuario = sessionStorage.getItem('usuario.id');
   //Lista todos Veiculos
-  $http.get('../App/Associado/listageral').success(function(data){
+  $http.get('../App/associado/listageral').success(function(data){
     $scope.veiculos = data;
-
   });
 
   //Lista todos Associado vinculado neste veiculo
