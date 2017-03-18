7b224a49ca4cc1d9a9c1084f803439703143f10a
Guilherme Brito <guilhermebritto.prof@gmail.com>
2017-03-18 15:53:08 -0300

    #6Front/Back - Upload de Imagem

--- App/Api/Controller/ImageController.php
+++ App/Api/Controller/ImageController.php
@@ -1,19 +1,53 @@
 <?php
 namespace Api\Controller;
-use \Api\lib\WideImage;
 class ImageController{
     
+    /**
+     * Metodo que gerencia o armazenamento de imagens
+     * 
+     * @return Array Com as informações do resultado
+     */
     public static function save(){
-        $upload = dirname(__FILE__).'/upload/';
-        var_dump($_FILES);
-        echo "<br>";
-        $img = WideImage::load($_FILES['arquivos']['tmp_name']);
-        $img = $img->resize(170, 180, 'outside');
-        $img = $img->crop('center', 'center', 170, 180);
-        $img->saveToFile($upload."teste.jpeg");
-        
-        
-    }
-
-
+        $destino = dirname(dirname(__FILE__)).'/upload/';
+        if($_FILES['arquivo']['name'] != '' && $$_FILES['arquivo']['nome']==0){
+            if($_FILES['arquivo']['size'] < 1000000){   // checa se o arquivo tem mais de 1MB
+                $arquivo_tmp = $_FILES['arquivo']['tmp_name'];
+                $nome = $_FILES['arquivo']['name'];
+                $extensao = pathinfo($nome, PATHINFO_EXTENSION); // retorna a extensão do arquivo
+                $extensao = strtolower($extensao);
+                if(strstr('.jpg;.jpeg;.gif;.png', $extensao)){
+                    $newName = uniqid(time()).".".$extensao;
+                    $destino .=$newName;
+                    if(@move_uploaded_file($arquivo_tmp, $destino)){
+                        return array(
+                            flag    => true,
+                            message => "Imagem salva com sucesso",
+                            path    => $destino,
+                            name    => $newName
+                        );
+                    }else{
+                        return array(
+                            flag    => false,
+                            message => "Imagem não pode ser salva. Aparentemente é um problema de escrita"
+                        );
+                    }
+                }else{
+                    return array(
+                        flag    => false,
+                        message => "Extensão não permitida. Somente as seguintes extensões são permitidas: JPG, JPEG, GIF, PNG"
+                    );
+                } 
+            }else{
+                return array(
+                    flag    => false,
+                    message => "Tamanho do arquivo exede o permitido"
+                );
+            }          
+        }else{
+            return array(
+                flag    => false,
+                message => "Nenhum arquivo foi enviado!"
+            );
+        }
+    }   
 }
--- App/route/testeRoute.php
+++ App/route/testeRoute.php
@@ -5,18 +5,20 @@ use \Psr\Http\Message\ResponseInterface as Response;
 $app->get('/test',function(Request $request, Response $response){
     echo "
         <table >
-        <form action='img' method='post' enctype='multipart/form-data'>
-            <input type='file' name='arquivos'  accept='image/png, image/jpeg'  multiple />
+        <form action='upload' method='post' enctype='multipart/form-data'>
+            <input type='file' name='arquivo'  accept='image/png, image/jpeg'>
+            <input type='text' name='texto'>
             <input type='submit' value='enviar'>
             </form>
         </table>
     ";
 });
 
-$app->post('/img',function(Request $request, Response $response){
-    echo \Api\Controller\ImageController::save();
-    echo "<img src='uploads/teste.jpeg' alt='UPLOAD' height='170' width='180'>";
+$app->get('/img',function(Request $request, Response $response){
+   echo "<img src='/gadeveloper/App/Api/upload/148985243658cd5814787e9.jpg'>";
+});
 
-
+$app->post('/upload', function(Request $request, Response $response){
+  var_dump($_FILES);
 });
 
