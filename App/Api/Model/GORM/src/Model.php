d1ad1cbcff9482d16797e8359b53bbdf69452a5b
Guilherme Brito <guilhermebritto.prof@gmail.com>
2017-04-11 23:07:25 -0300

    correcao

---
+++ App/Api/Model/GORM/src/Model.php
@@ -0,0 +1,98 @@
+<?php
+/**
+*	Classe desenvolvida com intuito de aprendizado, tentativa de gerar e entender o funcionamento de um ORM
+*	@author Guilherme Brito
+* 	@version 1.0
+*/
+namespace GORM;
+require_once(dirname(__DIR__).'/gorm.php');
+use PDO;
+class Model{
+	use Builder, Persistent, Finder;
+	/**
+	*	Variavel recebe uma instancia das classes filhas
+	*   @access protected
+	*	@name $class
+	*/
+	protected  		$class;
+	/**
+	* 	Variavel recebe o caminho completo da classe filha que há está chamando
+	* 	@access protected
+	*	@name $instance
+	*/
+	protected		$instance;
+	/**
+	* 	Variavel que recebe um array com as informações de conexão
+	* 	@access public
+	* 	@name $db
+	*/
+	public 		$db = array(
+		"driver" 	=> "mysql",
+		"host"		=> "localhost",		//31.220.104.130 mysql.hostinger.com.br
+		"dbname"	=> "u172775243_imobi", 		//u350275562_aetub u849106042_aetub
+		"user"		=> "imobiliar", 		//u350275562_aetub u849106042_aetub
+		"pass"		=> "imobiliar" 			//Z0kbFAWsap w8IJkSw25j8x
+	);
+	/**
+    * Recebe o nome da tabela da função Model::loadTable()
+    * @access public
+    * @name $table
+    */
+	public static $table;
+	/**
+	 * Informa qual é a primary key do objeto
+	 * 
+	 * @var String
+	 */
+	private static $primaryKey = null;
+	/**
+	 * Get PrimaryKey
+	 * 
+	 * @return String
+	 */
+	public function _getPrimaryKey(): String {
+		return $this->primaryKey;
+	}
+	/**
+	 * Set PrimaryKey
+	 * 
+	 * @param String $value
+	 * @return void
+	 */
+	public function _setPrimaryKey(String $value){
+		$this->primaryKey = $value;
+	}
+	/**
+	 * Retorna um Objeto da classe que está chamando
+	 * 
+	 * @param Array $array
+	 * @return Object
+	 */
+	public function load(Array $array = []){
+		$cls = get_called_class();
+		return new $cls($array);
+	}	
+	/**
+	*	Carrega a Tabela na variavel dentro da trait Builder::$table
+	*	@return void
+	*/
+	public function loadTable(){
+		$this->instance = get_called_class();
+		$this->table = str_replace("\\", "/", strtolower($this->instance));
+		$this->table = explode('/', $this->table);
+		$this->table = $this->table[count($this->table) -1];
+	
+	}
+	/**
+	* 	Retorna um ou mais objetos de seus respectivos tipos
+	* 	@param Array $return
+	* 	@return Array $array
+	*/
+	public function loadObject($return){
+		foreach ($return as $key => $value) {
+				$obj = new $this->instance($value);
+				$array[$key] =$obj->toArray();
+			}
+		return $array;
+	}
+}
