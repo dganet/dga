3e8235b9f5e9056fdc86ff02b86f2fd597e42ddd
adnilton <adniltonweb@gmail.com>
2017-04-29 17:58:33 -0300

    update

--- view/js/controller/osCtrl.js
+++ view/js/controller/osCtrl.js
@@ -2,23 +2,23 @@ app.controller("osCtrl", function($scope, $http, $timeout , $location	, $rootSco
     $scope.success = true;
     $scope.error = true;
     $scope.loadTecnicos = function(){
-        $http.get('/App/tecnico/list').then(function(response){
+        $http.get('/dga/App/tecnico/list').then(function(response){
             $scope.tecnicos = response.data;
         });
     }
     $scope.loadProblemas = function(){
-        $http.get('/App/problema/list').then(function(response){
+        $http.get('/dga/App/problema/list').then(function(response){
             $scope.problemas = response.data;
         });
     }
     $scope.loadBairros = function(){
-        $http.get('/App/bairro/list').then(function(response){
+        $http.get('/dga/App/bairro/list').then(function(response){
             $scope.bairros = response.data;
         });
     }
     
     $scope.loadServicos = function(){
-        $http.get('/App/servico/list').then(function(response){
+        $http.get('/dga/App/servico/list').then(function(response){
             $scope.servicos = response.data;
         });
     }
@@ -27,7 +27,7 @@ app.controller("osCtrl", function($scope, $http, $timeout , $location	, $rootSco
     $scope.addOs = function(dados){
         dados.fkTecnico = 0;
         dados.fkProblema = 0;
-        $http.post('/App/os/cadastrar', dados).then(function(response){
+        $http.post('/dga/App/os/cadastrar', dados).then(function(response){
             console.log(response.data);
             if(response.data == true ){
                 $scope.success = false;
@@ -35,17 +35,17 @@ app.controller("osCtrl", function($scope, $http, $timeout , $location	, $rootSco
 			               $scope.success = true;
 			    },5000);
             }else{
-                console.log("cheguei aqui 2");
+
                 $scope.error=false; 
             }
         });
     }
 
    
-        $http.get('/App/os/list').then(function(response){
+        $http.get('/dga/App/os/list').then(function(response){
             os = response.data;
             os.forEach(function(element) {
-                $http.get('/App/bairro/list/'+ os.fkBairro).then(function(response){
+                $http.get('/dga/App/bairro/list/'+ os.fkBairro).then(function(response){
                    element.fkBairro = response.data.nomeBairro
                 })    
             }, this);
