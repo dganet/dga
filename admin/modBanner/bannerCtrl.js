5fb371fb709e56f5ff0e4d5be5d89042ada58810
adnilton <adniltonweb@gmail.com>
2017-05-02 23:23:23 -0300

    updates banners

--- admin/modBanner/bannerCtrl.js
+++ admin/modBanner/bannerCtrl.js
@@ -5,16 +5,15 @@ var idUsuario = sessionStorage.getItem('usuario.id');
 $scope.mensagem = true;
 
         $http.get('../App/imagem/list').success(function(data){
-                 $scope.foto = data[0];  
-                 console.log(data);                
+                 $scope.foto = data[0];                
         });
 
         $http.get('../App/imagem/list').success(function(data){
-                  $scope.banner2 = data[1];      
+                  $scope.foto = data[1];      
         });
 
                 $http.get('../App/imagem/list').success(function(data){
-                  $scope.banner3 = data[2];      
+                  $scope.foto = data[2];      
         });
     //Upload Primeiro Banner 1
     $scope.upload1 = function(values){
