897c9256769a8dad0539a48727bcc539a6315739
adnilton <adniltonweb@gmail.com>
2018-01-05 18:49:03 -0200

    update

---
+++ view/js/controller/corretorCtrl.js
@@ -0,0 +1,6 @@
+app.controller('corretorCtrl', function($scope,restful){
+	$scope.busca = function(values){
+		console.log('estou aqui');
+		console.log(values);
+	}
+});
--- view/js/router/routeConfig.js
+++ view/js/router/routeConfig.js
@@ -26,6 +26,7 @@ app.config(function ($stateProvider, $urlRouterProvider, $locationProvider){
     .state('corretor', {
       url: '/corretor',
       templateUrl: 'view/site/corretor.htm',
+      controller:'corretorCtrl',
     })
 
     .state('cidade', {
