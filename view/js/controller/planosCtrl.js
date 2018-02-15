3ad39a20e1706724866df20c90818fd235cffea7
adnilton <adniltonweb@gmail.com>
2018-02-15 18:03:13 -0200

    update

--- view/js/controller/planosCtrl.js
+++ view/js/controller/planosCtrl.js
@@ -1,13 +1,25 @@
 app.controller('planosCtrl', function($scope, $http){
-	
-	var $urlPagSeguro = 'https://ws.sandbox.pagseguro.uol.com.br/v2/sessions?email=adniltonweb@gmail.com&token=C630DC7410554CE5A3EDA90BC6666A68';
+	var config = {headers: {
+		'Content-Type': 'application/json',
+		'Access-Control-Allow-Origin' : '*',
+		'Access-Control-Allow-Methods' : 'GET, POST, PUT, DELETE',
+		'X-Requested-With': 'XMLHttpRequest',
+		'Access-Control-Allow-Headers' : 'Origin, X-Requested-With, Content-Type, Accept'
+	}
+};
 
-	//$scope.idTransacaoPagSeguro = []; 
+	//https://ws.pagseguro.uol.com.br/recurring-payment/boletos?email=a@b.com&token=123AB
+	https://ws.sandbox.pagseguro.uol.com.br/v2/sessions?email=adniltonweb@gmail.com&token=C630DC7410554CE5A3EDA90BC6666A68
+	var $urlPagSeguro = 'https://ws.pagseguro.uol.com.br/recurring-payment/boletos?email=adniltonweb@gmail.com&token=C630DC7410554CE5A3EDA90BC6666A68';
+
+
 
 	
-		$http.post($urlPagSeguro).success(function(response){
+		$http.post($urlPagSeguro, config).success(function(response){
 			//Id Transação PagSeguro
-			this.idTransacaoPagSeguro = response;	
+			console.log(response);	
 
 		});
+
+
 	});
